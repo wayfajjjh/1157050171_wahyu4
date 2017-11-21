7f952314c73730745cbabc3f30e820c1c7951ce4
wayfajjjh <wahyubarat8@gmail.com>
2017-11-21 07:21:55 +0700

    baru neeh

--- coba.php
+++ coba.php
@@ -3,38 +3,207 @@
 	<title></title>
 </head>
 <body>
-nalifneuinfeui ifniuanFe jfieafiuenf fiufiua ei j dihwaudaudwb duiawduib daiudgwuda dwauibw ad w
-</body>
-</html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-nalifneuinfeui ifniuanFe jfieafiuenf fiufiua ei j dihwaudaudwb duiawduib daiudgwuda dwauibw ad w
-</body>
-</html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-nalifneuinfeui ifniuanFe jfieafiuenf fiufiua ei j dihwaudaudwb duiawduib daiudgwuda dwauibw ad w
-</body>
-</html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-nalifneuinfeui ifniuanFe jfieafiuenf fiufiua ei j dihwaudaudwb duiawduib daiudgwuda dwauibw ad w
-</body>
-</html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-nalifneuinfeui ifniuanFe jfieafiuenf fiufiua ei j dihwaudaudwb duiawduib daiudgwuda dwauibw ad w
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+   <div class="container">
+           
+    <!-- untuk menu -->
+   <div class="row">
+     <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Accme Widget</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+   </div>
+  <!--  Gambar -->
+   <div class="row">
+     <div class="col-md-12">
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+       <img src="img/ing.jpg" alt="..." style="width: 100%;height: 500px">
+      <div class="carousel-caption">
+       Gedung Rektorat UIN
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/anwar.jpg" alt="..." style="width: 100%;height: 500px">
+      <div class="carousel-caption">
+      UIN Bandung maju
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/sains.jpg" alt="..." style="width: 100%;height: 500px">
+      <div class="carousel-caption">
+      </div>
+        Informatika UIN The Best
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+       
+     </div>
+   </div>
+   <div class="row">
+     <div class="col-md-12">
+       <h2>Intro Copy Headline</h2>
+     </div>
+     <p>Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet
+     Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet
+     Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet Lorem Ipsum dolor is asmet</p>
+   </div>
+   <div class="row">
+     <div class="col-md-3 col-xs-6">
+       <img src="img/ing.jpg" alt="..." style="width: 100%;height: 300px">
+       <h3>Judul Berita</h3>
+       <p>Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita  </p>
+     </div>
+      <div class="col-md-3 col-xs-6">
+        <img src="img/sains.jpg" alt="..." style="width: 100%;height: 300px">
+        <h3>Judul Berita</h3>
+       <p>Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita  </p>
+      </div>
+       <div class="col-md-3 col-xs-6">
+         <img src="img/anwar.jpg" alt="..." style="width: 100%;height: 300px">
+         <h3>Judul Berita</h3>
+       <p>Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita  </p>
+       </div>
+        <div class="col-md-3 col-xs-6">
+          <img src="img/ing.jpg" alt="..." style="width: 100%;height: 300px">
+          <h3>Judul Berita</h3>
+          <p>Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita Deskripsi Berita Deskripsi Berita  Deskripsi Berita  </p>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <h3>Quick Link</h3>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Event</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Support</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <h3>Announcement and Promoted Content</h3>
+            <p>Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten
+            Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten
+          Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten Deskripsi konten</p>
+          </div>
+          <div class="col-md-3">
+            <h3>Coming soon</h3>
+            <img src="img/ing.jpg" alt="..." style="width: 100%;height: 100px">
+          </div>
+        </div>
+
+   </div>
+            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Sign In</button>
+      </div>
+    </div>
+  </div>
+</div>
+   </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html></body>
 </html>
